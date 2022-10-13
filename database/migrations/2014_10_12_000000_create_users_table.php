18b3a23711f4d8099d36c44afb812c00ffd9c435
Irisia <pochara.su@ku.th>
2022-10-13 20:54:14 +0700

    user table add role

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('role')->default('USER');
             $table->bigInteger('point')->default(0);
             $table->string('image_path')->nullable()->default(null);
             $table->timestamp('email_verified_at')->nullable();
