bb343c3e63abc4d3d01f35bc057c72d4dd370ae9
Irisia <pochara.su@ku.th>
2022-11-07 05:27:50 +0700

    save

--- database/migrations/2022_11_06_152102_create_reports_table.php
+++ database/migrations/2022_11_06_152102_create_reports_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('reports', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(\App\Models\Product::class);
+            $table->foreignIdFor(\App\Models\User::class);
+            $table->string('topic');
+            $table->string('detail')->nullable();
             $table->timestamps();
         });
     }
