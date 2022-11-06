69d387dfbcf6bbbbc6b518602bdd24ce20d1e9fd
Irisia <pochara.su@ku.th>
2022-11-06 16:17:13 +0700

    up

--- database/seeders/ReviewSeeder.php
+++ database/seeders/ReviewSeeder.php
@@ -17,8 +17,8 @@ class ReviewSeeder extends Seeder
      */
     public function run()
     {
-        $this->command->line("Generating 100 review");
-        Review::factory(100)->create();
+        $this->command->line("Generating 1000 review");
+        Review::factory(1000)->create();
         foreach (Review::get() as $review) {
             $product = Product::find($review->product_id);
             $product->rating = Review::where('product_id', $product->id)->sum('rating') / Review::where('product_id', $product->id)->count();
